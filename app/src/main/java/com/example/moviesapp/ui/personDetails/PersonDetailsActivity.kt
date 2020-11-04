b1042701e061165cc7dc87ef6676bb90d1d3b955
Elshad98 <safarov.elshad007@gmail.com>
2020-11-04 23:55:36 +0500

    Updated the UI for person details

--- app/src/main/java/com/example/moviesapp/ui/personDetails/PersonDetailsActivity.kt
+++ app/src/main/java/com/example/moviesapp/ui/personDetails/PersonDetailsActivity.kt
@@ -1,17 +1,43 @@
 package com.example.moviesapp.ui.personDetails
 
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
+import com.example.moviesapp.App
+import com.example.moviesapp.BuildConfig.POSTER_BASE_URL
 import com.example.moviesapp.R
+import com.example.moviesapp.data.repository.NetworkState
+import com.example.moviesapp.data.vo.PersonDetails
 import com.example.moviesapp.ui.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_person_details.*
 
 class PersonDetailsActivity : BaseActivity() {
+
+    private lateinit var viewModel: PersonDetailsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_person_details)
 
         val personId = intent.getIntExtra("person_id", 1)
-        Log.d("PersonDetailsActivity", "Person id: $personId")
+
+        viewModel = viewModel(PersonDetailsViewModel::class.java, App.scope())
+        viewModel.getPersonDetails(personId).observe(this, Observer { bindUI(it) })
+        viewModel.getNetworkState().observe(this, stateObserver)
+    }
+
+    private val stateObserver = Observer<NetworkState> { networkState ->
+        progress_bar_person.visibility =
+            if (networkState == NetworkState.LOADING) View.VISIBLE else View.GONE
+        txt_error_person.visibility =
+            if (networkState == NetworkState.ERROR) View.VISIBLE else View.GONE
+    }
+
+    private fun bindUI(personDetails: PersonDetails) {
+        person_name.text = personDetails.name
+
+        val personImageURL = POSTER_BASE_URL + personDetails.profilePath
+        Glide.with(this).load(personImageURL).into(iv_person_image)
     }
 }
